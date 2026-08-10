33e6855e22b0fab4ff490eefab9cf39eb3ab806b
su59201 <su59201@gmail.com>
2026-08-10 10:52:11 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>BRANDON BAEK</h1>
+      <h2>Global Business Builder & Strategic Problem Solver</h2>
+      <p>
+        I build practical solutions for complex business problems.
+      </p>
+    </main>
+  );
+}
