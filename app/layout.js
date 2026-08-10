3a41322779d026cf2194db8b0fd691283d1f4f49
su59201 <su59201@gmail.com>
2026-08-10 10:53:46 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Brandon Baek",
+  description: "Global Business Builder & Strategic Problem Solver",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
